26a4b0d0c154ecfb9cd7f8891ed6d3f8103fdb5f
Igor Veremsky <igorveremsky@gmail.com>
2018-02-21 16:14:13 +0200

    add structure for PHPDoc

--- components/SeoPatternHelper.php
+++ components/SeoPatternHelper.php
@@ -8,10 +8,21 @@ use yii\base\Model;
 /**
  * SeoPatternHelper is the pattern helper class add feature for use patterns in model seo values.
  *
+ * Class structure:
+ * 1. Constants and class variables
+ * 2. Get variables functions
+ * 3. Helper functions
+ * 4. Public functional functions
+ * 5. Protected functional functions
+ * 6. Patterns retrieved functions
+ * 7. Sanitiezed functions
+ *
  * @author Igor Veremsky <igor.veremsky@gmail.com>
  * @since x.x.x
  */
 class SeoPatternHelper {
+	/* *********************** CONSTANTS AND VARIABLES ************************** */
+
 	/**
 	 * Pattern prefix for represents that its pattern need to be replace with model attribute.
 	 */
@@ -22,31 +33,19 @@ class SeoPatternHelper {
 	 */
 	const PATTERN_DELIMETER = '%%';
 
+	/* *********************** GET VARIABLES FUNCTIONS ************************** */
+
 	/**
-	 * Sanitize string that contains patterns.
-	 *
-	 * @param string $patternString
+	 * Returns pattern regular expression for find patterns in string.
 	 *
 	 * @return string
 	 */
-	protected static function sanitizePatternString($patternString) {
-		$patternString = strip_tags($patternString);
-
-		return $patternString;
+	protected static function getPatternRegExp() {
+		$patternDelimeter = self::PATTERN_DELIMETER;
+		return '/'.$patternDelimeter.'([^'.$patternDelimeter[0].']+)'.$patternDelimeter.'?/iu';
 	}
 
-	/**
-	 * Sanitize string that generated after replace patterns with theirs values.
-	 *
-	 * @param $replacedString
-	 *
-	 * @return string
-	 */
-	protected static function sanitizeReplacedString($replacedString) {
-		$replacedString = trim($replacedString);
-
-		return $replacedString;
-	}
+	/* *********************** HELPER FUNCTIONS ************************** */
 
 	/**
 	 * Add patterns delimeters for pattern key.
@@ -70,46 +69,7 @@ class SeoPatternHelper {
 		return str_replace(self::MODEL_ATTRIBUTE_PATTERN_PREFIX, '', $patternKey);
 	}
 
-	/**
-	 * Returns pattern regular expression for find patterns in string.
-	 *
-	 * @return string
-	 */
-	protected static function getPatternRegExp() {
-		$patternDelimeter = self::PATTERN_DELIMETER;
-		return '/'.$patternDelimeter.'([^'.$patternDelimeter[0].']+)'.$patternDelimeter.'?/iu';
-	}
-
-	/**
-	 * Returns array with patterns finded in string.
-	 *
-	 * @param $patternString
-	 *
-	 * @return mixed
-	 */
-	public static function findPatterns($patternString) {
-		$patternString = self::sanitizePatternString($patternString);
-
-		$patternRegExp = self::getPatternRegExp();
-		preg_match_all($patternRegExp, $patternString, $patternsMatches);
-
-		return $patternsMatches[1];
-	}
-
-	/**
-	 * Returns model attribute compared with pattern key.
-	 * If model don`t have such attribute returns empty string.
-	 *
-	 * @param Model $model
-	 * @param $patternKey
-	 *
-	 * @return mixed|string
-	 */
-	public static function retrieveModelAttribute(Model $model, $patternKey) {
-		$modelAttributeName = self::getModelAttributeNameFromPatternKey($patternKey);
-
-		return (property_exists($model, $modelAttributeName)) ? $model->{$modelAttributeName} : '';
-	}
+	/* *********************** PUBLIC FUNCTIONAL FUNCTIONS ************************** */
 
 	/**
 	 * Function that replace patterns with theirs values.
@@ -145,6 +105,69 @@ class SeoPatternHelper {
 		}
 
 		$replacedString = self::sanitizeReplacedString($replacedString);
+
+		return $replacedString;
+	}
+
+	/* *********************** PROTECTED FUNCTIONAL FUNCTIONS ************************** */
+
+	/**
+	 * Returns array with patterns finded in string.
+	 *
+	 * @param $patternString
+	 *
+	 * @return mixed
+	 */
+	protected static function findPatterns($patternString) {
+		$patternString = self::sanitizePatternString($patternString);
+
+		$patternRegExp = self::getPatternRegExp();
+		preg_match_all($patternRegExp, $patternString, $patternsMatches);
+
+		return $patternsMatches[1];
+	}
+
+	/* *********************** PATTERNS RETRIEVED FUNCTIONS ************************** */
+
+	/**
+	 * Returns model attribute compared with pattern key.
+	 * If model don`t have such attribute returns empty string.
+	 *
+	 * @param Model $model
+	 * @param $patternKey
+	 *
+	 * @return mixed|string
+	 */
+	public static function retrieveModelAttribute(Model $model, $patternKey) {
+		$modelAttributeName = self::getModelAttributeNameFromPatternKey($patternKey);
+
+		return (property_exists($model, $modelAttributeName)) ? $model->{$modelAttributeName} : '';
+	}
+
+	/* *********************** SANITIZIED FUNCTIONS ************************** */
+
+	/**
+	 * Sanitize string that contains patterns.
+	 *
+	 * @param string $patternString
+	 *
+	 * @return string
+	 */
+	protected static function sanitizePatternString($patternString) {
+		$patternString = strip_tags($patternString);
+
+		return $patternString;
+	}
+
+	/**
+	 * Sanitize string that generated after replace patterns with theirs values.
+	 *
+	 * @param $replacedString
+	 *
+	 * @return string
+	 */
+	protected static function sanitizeReplacedString($replacedString) {
+		$replacedString = trim($replacedString);
 
 		return $replacedString;
 	}
